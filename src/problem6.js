9ef3dc2840cdd9f9464ab737c0c00e5b0303df9d
LeeYejin013 <remitjoy0413@gmail.com>
2024-10-28 17:56:33 +0900

    6번 문제입니다

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,32 @@
 function problem6(forms) {
-  var answer;
+  var answer = [];
+  var info = new Map();
+  var same;
+  var sameSet = new Set();
+  var dup = new Map();
+  for(const [email, name] of forms) {
+    info.set(email, name);
+  }
+
+  for(var [email, name] of info){
+    for (var i=0; i<name.length-1; i++) {
+      for (var j=2; j<=name.length-i; j++) {
+        same = name.slice(i, i+j);
+
+        if (dup.has(same)) {
+          sameSet.add(dup.get(same));
+          sameSet.add(email);
+        }
+        else{
+          dup.set(same, email);
+        }
+      }
+    }
+  }
+  for(let email of sameSet){
+    answer.push(email);
+  }
+  answer = answer.sort();
   return answer;
 }
 
