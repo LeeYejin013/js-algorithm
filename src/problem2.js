edf77c3f6b071f7f5b768b729421764455b8bdc9
LeeYejin013 <remitjoy0413@gmail.com>
2024-10-28 17:55:43 +0900

    2번 문제입니다

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,23 @@
 function problem2(cryptogram) {
   var answer;
+  var word = "";
+  var string = cryptogram.split("");
+  var strlen = 0;
+
+  while(string.length!=strlen){
+    strlen = string.length;
+    for(var i=1; i<string.length; i++){
+      if(string[i-1] == string[i]){
+        string.splice(i-1, 2);
+      }
+    }
+  }
+
+  for(var j=0; j<string.length; j++){
+    word = word + string[j];
+  }
+  answer = word;
+
   return answer;
 }
 
